d2d0d604765ea6fbded0013367572444cd8d2a64
Robert Ableson <robertableson@gmail.com>
2022-06-09 20:39:38 -0400

    Adjusted readme + added list in php file

---
+++ dog-breeds.php
@@ -0,0 +1,425 @@
+<?php
+
+$dog_breeds = [
+    [
+        'breed_name' => 'Other',
+        'bed_size' => '0'
+    ],
+    [
+        'breed_name' => 'Affenpinscher',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Airedale Terrier',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Afghan Hound',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Akita',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'American Eskimo',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'American Bulldog',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'American Staffordshire Terrier',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Australian Cattle Dog',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Australian Shepherd',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Australian Terrier',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Basenji',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Basset Hound',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Beagle',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Belgian Tervuren/Sheepdog',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Bernese Mountain Dog',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'Bloodhound',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Bichon Frise',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Bichon Maltese',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Border Collie',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Boston Terrier',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Boxer',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Brussels Griffon',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Bull Terrier',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Bulldog/Bullmastiff',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'Cairn Terrier',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Cavapoo/Cavoodle/Cavador',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Carolina Dog',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Cavalier King Charles Spaniel',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Chihuahua/Chi Chi',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Chinook',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'Cocker Spaniel',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Cockapoo',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Corgi',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Collie',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Dachshund',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Dalmatian',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'Doberman Pinscher',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'Dogue De Bordeaux',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'English Cocker Spaniel',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'English Foxhound',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'English Springer Spaniel',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Finnish Spitz',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Fox Terrier',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'French Bulldog',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'French Spaniel',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'German Shepherd',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'German Pinscher',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'German Pointer',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'German Spitz',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Golden Retriever',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Golden Cocker',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Goldendoodle',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Gordon Setter',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Greyhound',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'Harrier/Havanese',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Hokkaido',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Huntaway',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Irish Terrier/Setter/Water Spaniel',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Jack Russell',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Jack Russell Terrier',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Japanese Spitz',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'King Charles Spaniel',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Labrador Retreiver',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Labrador',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Lab Pointer',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Labradoodle (Standard)',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'Labradoodle (Medium)',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Labradoodle (Miniature)',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Löwchen',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Maltese',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Maltese Shih Tzu',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Maltipoo/Morkie',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Mastiff',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'Miniature Dachshund',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Miniature Pinscher',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Miniature Schnauzer',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Norfolk/Norwich Terrier',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Old English Bulldog',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Otterhound',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Papillon',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Pomeranian',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Pembroke Welsh Corgi',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Pointer',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Poodle (Standard)',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Poodle (Toy/Mini)',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Pug',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Rottweiler',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Saint Bernard',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'Samoyed/Saluki',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'Schnoodle',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Scottish Terrier',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Spoodle',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Springer Spaniel',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Shih Tzu',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Shichon',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Shiba Inu',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Siberian Husky',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'Staffy (Staffordshire)',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Toy Poodle',
+        'bed_size' => 's'
+    ],
+    [
+        'breed_name' => 'Terrier',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Vizsla',
+        'bed_size' => 'xl'
+    ],
+    [
+        'breed_name' => 'Welsh Terrier',
+        'bed_size' => 'm'
+    ],
+    [
+        'breed_name' => 'Whippet',
+        'bed_size' => 'l'
+    ],
+    [
+        'breed_name' => 'Yorkshire Terrier',
+        'bed_size' => 'm'
+    ]
+
+];
